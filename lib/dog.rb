e1ee4bd65a1a3dbe4d6d6a85d336eb4cd37a73f6
Jay Bednar <ironviralmedia@gmail.com>
2018-01-27 15:57:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,12 @@
 class Dog
-  
+
   def intialize(name, breed = "Mutt")
     @name = name
     @breed = breed
   end
 
-  def name=(name)
-    @name = name
-  end
-
   def name
     @name
-  end
-
-  def breed=(breed)
-    @breed = breed
   end
 
   def breed
